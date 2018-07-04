d21da75e649110014572e4dc55fc1f754995d6d7
ALM <agathe_lm12@hotmail.com>
2018-07-04 12:21:18 +0200

    ajout td

---
+++ tpTodoList/App.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+/* TP TodoList*/
+
+class App extends Component{
+    render(){
+		return(
+		
+				<div>				
+					<h1> TP Todolist </h1>	
+				</div>
+		);
+	}
+} 
+
+export default App ;
+
+
---
+++ tpTodoList/reducers/reducers.jsx
@@ -0,0 +1,31 @@
+import {combineReducers} from 'redux';
+import {ADD_TODO} from '../actions/actions';
+
+/* Les actions déclenchent uniquement des modifications dans l'application */
+function todo(state,action){
+	switch(action.type){
+		case ADD_TODO:
+			return{
+				id: action.id,
+				text: action.text
+			}
+		default:
+			return state
+	}
+}
+
+function todos(state=[],action){
+	switch(action.type){
+		case ADD_TODO:
+			return [
+				...state,
+				todo(undefined,action)
+			]
+		default:
+			return state
+	}
+}
+
+const todoApp = combineReducers({
+	todos
+}]
